0a6f7ed95b21491e9fd4f2b88db47c933e895a30
Daníel Örn Árnason <danielarnason@Daniels-MacBook-Air.local>
2016-04-28 18:11:45 +0200

    lavet en csv branch, til at afprøve Python3, der kan håndtere csv filer med danske bogstaver.

--- adressevasker.py
+++ adressevasker.py
@@ -1,46 +1,26 @@
 # coding: utf-8
-
+#Dette script skal køres i Python 3
 #Dette script er en test af datavask tjenesten fra dawa.aws.dk. Skriv en adresse og få returneret den rigtige adresse, vejkode og koordinater
-import urllib
+import urllib.parse
+import urllib.request
 import json
+import csv
 
 service_url ='http://dawa.aws.dk/datavask/adresser?'
 
-while True:
-	adr = raw_input('Skriv en adresse - ')	
-	if len(adr) < 1:
-		print 'Du skal skrive noget'
+with open('/Users/danielarnason/Documents/adresse.csv', 'r') as csv_file:
+	for row in csv_file:
+		url = service_url + urllib.parse.urlencode({'betegnelse' : row})
+		url_data = urllib.request.urlopen(url).read()
+		js_data = json.dumps(url_data)
+	print(js_data)
 
-	url = service_url + urllib.urlencode({'betegnelse' : adr})
-	url_data = urllib.urlopen(url).read()
+	# for row in reader:
+	# 	print row.encode('utf-8')
 
-	js_data = json.loads(url_data)
-
-	#data fra adresseid link
-	adr_url = js_data['resultater'][0]['aktueladresse']['href']
-	adr_url_data = urllib.urlopen(adr_url).read()
-	adresse_js = json.loads(adr_url_data)
-
-	#nøjagtighed
-	kategori = js_data['kategori']
-
-
-	#print statements
-	if kategori == 'B':
-		print 'Kategori B: Sikkert match'
-	elif kategori == 'C':
-		print 'Kategori C: Usikkert match'
-	else:
-		print 'Kategori A: Præcist match'
-	#print 'Kategori: ', js_data['kategori']
-	#print 'Vejnavn: ', js_data['resultater'][0]['aktueladresse']['vejnavn']
-	#print 'Husnr: ', js_data['resultater'][0]['aktueladresse']['husnr']
-	#print 'Postnr: ', js_data['resultater'][0]['aktueladresse']['postnr']
-	print 'Rigtig adresse: ', adresse_js['adressebetegnelse']
-	print 'Vejkode: ', adresse_js['adgangsadresse']['vejstykke']['kode']
-	print 'Koordinater: ', adresse_js['adgangsadresse']['adgangspunkt']['koordinater'][0], adresse_js['adgangsadresse']['adgangspunkt']['koordinater'][1]
-
-	#print json.dumps(js_data, indent=4)
-	
-
-
+	#
+	# url = service_url + urllib.urlencode({'betegnelse' : adr})
+	# url_data = urllib.urlopen(url).read()
+	#
+	# js_data = json.loads(url_data)
+	# print js_data
